650088ea825fc88b9b9b04d1e1d4dddfe9aae65f
Lucag23 <gennarelli.1919725@studenti.uniroma1.it>
2022-05-31 01:58:10 +0200

    username univoco

--- login/login.php
+++ login/login.php
@@ -11,14 +11,15 @@ $result = $mysqli->query($query) or die( "Unable to query");
 $num = mysqli_num_rows($result);
 $logdb = mysqli_fetch_row($result);
 $mysqli->close();
-$username = $logdb['5'];
+$username = "SELECT username FROM utenti WHERE email = '$email' AND password = '$password'";
 
 if ($num < 1) {
 $erroreNonEsiste = "1";
-echo 'email o password non validi';
+echo 'email o password non validi'; 
 } else {
     setcookie("username", $username);
-    echo 'Benvenuto';
+    echo "Benvenuto", $username;
+    echo "<a href='../home/home con login.php'> <button> vai alla home </button></a>";
 }
 
 
--- registrazione/registrazione.php
+++ registrazione/registrazione.php
@@ -10,8 +10,8 @@ $cap = $_POST['cap'];
 $indirizzo = $_POST['indirizzo'];
 $civico = $_POST['civico'];
 if($password != $password2){
-    echo('password non coincidono     ');
-    echo('<a href=registrazione.html> Clicca qui per riprovare </a>');
+    echo "password non coincidono";
+    echo "<a href=registrazione.html> Clicca qui per riprovare </a>";
 }else{
     $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $db) or die( "Unable to connect");
     $mysqli->select_db($db) or die( "Unable to select database");
@@ -19,17 +19,25 @@ if($password != $password2){
     $query="SELECT * FROM utenti WHERE email = '$email'";
     $result = $mysqli->query($query) or die( "Impossibile utilizzare la query");
     $num = mysqli_num_rows($result);
+    $query="SELECT * FROM utenti WHERE username = '$username'";
+    $result = $mysqli->query($query) or die( "Impossibile utilizzare la query");
+    $num2 = mysqli_num_rows($result);
 
     if ($num > 0) {
     $erroreMailUsata = "1";
     echo('Esiste già una registrazione con questa email ');
     echo('<a href=registrazione.html> Clicca qui per riprovare </a>');
+    }if ($num2 > 0) {
+        $erroreMailUsata2 = "1";
+        echo('Esiste già una registrazione con questo username ');
+        echo('<a href=registrazione.html> Clicca qui per riprovare </a>');
     } else {
-        $query= "INSERT INTO utenti (email, password, nome, cognome, cap, indirizzo, civico) VALUES ('$email','$password', '$nome','$cognome','$cap','$indirizzo','$civico')";
+        $query= "INSERT INTO utenti (email, password, nome, cognome, username, cap, indirizzo, civico) VALUES ('$email','$password', '$nome', '$cognome', '$username', '$cap', '$indirizzo', '$civico')";
         $mysqli->query($query) or die( "Unable to query");
         echo "registrazione effettuata";
-}
+        echo "<a href='../login/accedi.html'> <button> vai alla pagina d'accesso </button></a>";
 
+}
 $mysqli->close();
 }
    
