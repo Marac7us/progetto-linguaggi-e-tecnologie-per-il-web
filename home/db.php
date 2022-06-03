8d3ccfa033fef76d8276ae799e1e6ac7ccafc013
Marac7us <104776166+Marac7us@users.noreply.github.com>
2022-06-03 15:33:51 +0200

    ultimo commit

--- home/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$con = mysqli_connect("localhost:3306","user","admin","trialbio");
-	if (mysqli_connect_errno()){
-		echo "Failed to connect to MySQL: " . mysqli_connect_error();
-		die();
-		}
-?>
